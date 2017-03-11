34619176039dd0e17fdbf33babedc6b776d64a86
qq960826 <qq960826@gmail.com>
2017-03-11 20:32:17 +0800

    Add python protocol

---
+++ Protocol/Mainfold/Python/protocol.py
@@ -0,0 +1,23 @@
+import socket
+import protocol_pb2 as protobuf
+class Protocol:
+    def __init__(self,address,port):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.address=(address,int(port))
+    def SendToUDP(self,data):
+        self.socket.sendto(data,self.address)
+    def Send(self,datatype,data):
+        res=protobuf.Message()
+        res.messagetype=datatype
+        res.data=data
+        self.SendToUDP(res.SerializeToString())
+    def SendPadPass(self,pad,password):
+        res=protobuf.PadPass()
+        res.pad=pad
+        res.password=password
+        self.Send(protobuf.Message.PadPass,res.SerializeToString())
+
+
+a=Protocol('192.168.1.107',6000)
+#a.Send(protobuf.Message.PADPASS,"wzq")
+a.SendPadPass("528796413","5897")
---
+++ Protocol/Protobuf/autogen.sh
@@ -0,0 +1,4 @@
+protoc-c --c_out=. protocol.proto
+protoc --csharp_out=. protocol.proto
+protoc --cpp_out=. protocol.proto
+protoc --python_out=. protocol.proto
