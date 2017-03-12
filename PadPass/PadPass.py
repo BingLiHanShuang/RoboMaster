50f4862c48ec8349ab4e82f720fd1faefdb0f3f6
qq960826 <qq960826@gmail.com>
2017-03-12 12:46:39 +0800

    Add test image

--- PadPass/PadPass.py
+++ PadPass/PadPass.py
@@ -4,9 +4,9 @@ import protocol
 import DigitRecognization
 import cv2
 
-datainterface=protocol.Protocol('192.168.1.107',6000)
+datainterface=protocol.Protocol('192.168.1.200',6000)
 
-raw_image = cv2.imread("/Users/wzq/Downloads/testcase1/363_92785372614985.jpg")
+raw_image = cv2.imread("363_92785372614985.jpg")
 res= DigitRecognization.recognize(raw_image)
 print res
 datainterface.SendPadPass(res,str(12345))
