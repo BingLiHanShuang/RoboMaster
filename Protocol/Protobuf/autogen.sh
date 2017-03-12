33dd9410e43c79ab53a09a22b3034a9049ab0cd2
qq960826 <qq960826@gmail.com>
2017-03-12 12:27:57 +0800

    Add PadPass

--- Protocol/Protobuf/autogen.sh
+++ Protocol/Protobuf/autogen.sh
@@ -1,4 +1,4 @@
-protoc-c --c_out=. protocol.proto
+protoc-c --c_out=../Mainfold/stm32-protocol/ protocol.proto
 protoc --csharp_out=. protocol.proto
 protoc --cpp_out=. protocol.proto
-protoc --python_out=. protocol.proto
+protoc --python_out=../Mainfold/Python protocol.proto
