798c75777d828c64b02690fcb7d0ea246a675216
qq960826 <qq960826@gmail.com>
2017-03-24 14:13:13 +0800

    Change

--- Protocol/Protobuf/autogen.sh
+++ Protocol/Protobuf/autogen.sh
@@ -1,4 +1,5 @@
 protoc-c --c_out=../stm32-protocol/ protocol.proto
+protoc-c --c_out=../Mainfold/SerialInterface/ protocol.proto
 protoc --csharp_out=. protocol.proto
 protoc --cpp_out=. protocol.proto
 protoc --python_out=../Mainfold/Python protocol.proto
