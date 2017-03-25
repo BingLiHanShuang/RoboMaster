55c977c2f5495389d7a17f134728b1f50f20e6da
qq960826 <qq960826@gmail.com>
2017-03-25 13:39:34 +0800

    implement VideiRecording

---
+++ VideoCapture/cmake-build-debug/CMakeFiles/VideoCapture.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/parallels/RoboMaster/VideoCapture/protobuf-c.c" "/home/parallels/RoboMaster/VideoCapture/cmake-build-debug/CMakeFiles/VideoCapture.dir/protobuf-c.c.o"
+  "/home/parallels/RoboMaster/VideoCapture/protocol.pb-c.c" "/home/parallels/RoboMaster/VideoCapture/cmake-build-debug/CMakeFiles/VideoCapture.dir/protocol.pb-c.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/parallels/RoboMaster/VideoCapture/ReceiveHandler.cpp" "/home/parallels/RoboMaster/VideoCapture/cmake-build-debug/CMakeFiles/VideoCapture.dir/ReceiveHandler.cpp.o"
+  "/home/parallels/RoboMaster/VideoCapture/UDPClient.cpp" "/home/parallels/RoboMaster/VideoCapture/cmake-build-debug/CMakeFiles/VideoCapture.dir/UDPClient.cpp.o"
+  "/home/parallels/RoboMaster/VideoCapture/UDPServer.cpp" "/home/parallels/RoboMaster/VideoCapture/cmake-build-debug/CMakeFiles/VideoCapture.dir/UDPServer.cpp.o"
+  "/home/parallels/RoboMaster/VideoCapture/main.cpp" "/home/parallels/RoboMaster/VideoCapture/cmake-build-debug/CMakeFiles/VideoCapture.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
