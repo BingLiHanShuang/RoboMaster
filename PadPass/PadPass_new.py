0e753895618b75ab95deed0d4d970e529e57a1d0
wzq <qq960826@gmail.com>
2017-03-26 14:46:17 +0800

    update image process

--- PadPass/PadPass_new.py
+++ PadPass/PadPass_new.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import datetime
 def resize(rawimg):  # resize img to 28*28
     fx = 640.0 / rawimg.shape[0]
     fy = 480.0 / rawimg.shape[1]
@@ -31,7 +32,10 @@ green = np.uint8([[[110,140,130 ]]])
 hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
 print hsv_green
 
-frame=cv2.imread("/Users/wzq/Downloads/MobileFile/IMG_20170317_205803.jpg")
+
+frame=cv2.imread("/Users/wzq/Downloads/MobileFile/IMG_20170317_205659.jpg")
+begin = datetime.datetime.now()
+
 frame=resize(frame)
 frame = cv2.GaussianBlur(frame, (5, 5), 1)
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -68,22 +72,83 @@ mask=mask[image_dict[1]:image_dict[1]+image_dict[3],image_dict[0]:image_dict[0]+
 
 contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # peri = cv2.arcLength(c, True)
+count=0
+sum_angle=0
 for cnt in contours:
     approx = cv2.approxPolyDP(cnt, 0.04 * cv2.arcLength(cnt, True), True)
     if len(approx) == 4 or len(approx)==5:
         rect= cv2.minAreaRect(cnt)
         #w,h,x,y=int(rect[0][1]),int(rect[0][1]),int(rect[1][0]),int(rect[1][1])
         h,w=rect[1][0],rect[1][1]
-        if  w>30 or h> 30 or w*h<160:
+        if  w*h<160 or w*h>340:
             continue
         # if(rect[1])
-        print w,h,w*h
+        sum_angle+=rect[2]
+        count+=1
+        #print w,h,w*h,rect[2]
         box = cv2.cv.BoxPoints(rect)
         box = np.int0(box)
-        cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
-        # image_dict.append([x, y, w, h])
+        #cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
+        #image_dict.append([x, y, w, h])
         #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
+RotateMatrix = cv2.getRotationMatrix2D(center=(frame.shape[1]/2, frame.shape[0]/2), angle=sum_angle/count+90, scale=1)
+frame = cv2.warpAffine(frame, RotateMatrix, (frame.shape[0]*2, frame.shape[1]*1))
+mask = cv2.warpAffine(mask, RotateMatrix, (frame.shape[0]*2, frame.shape[1]*1))
+
+
+contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+min_y=0
+avg_width=0
+avg_height=0
+pos_rect=[]
+for cnt in contours:
+    approx = cv2.approxPolyDP(cnt, 0.04 * cv2.arcLength(cnt, True), True)
+    if len(approx) == 4 or len(approx)==5:
+        x, y, w, h = cv2.boundingRect(cnt)
+        #w,h,x,y=int(rect[0][1]),int(rect[0][1]),int(rect[1][0]),int(rect[1][1])
+        if  w*h<160 or w*h>380:
+            continue
+        # if(rect[1])
+        avg_width+=w
+        avg_height+=h
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        pos_rect.append([x,y,w,h])
+
+        #image_dict.append([x, y, w, h])
+        #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+pos_rect.sort(key=lambda x:(x[0],x[1]))
+avg_width/=10
+avg_height/=10
+avg_x_left=sum([float(i[0]) for i in pos_rect[0:5]])/5
+avg_x_right=sum([float(i[0]) for i in pos_rect[5:10]])/5
+
+pos_rect.sort(key=lambda x:(x[1]))
+avg_y_top=sum([float(i[1]) for i in pos_rect[0:2]])/2
+
+avg_x_left+=avg_width
+print avg_x_left,avg_x_right,avg_width,avg_height
+print pos_rect
+
+length=avg_x_right-avg_x_left
+
+x=0.147703180212014*length+avg_x_left
+y=avg_y_top
+h=2.164948453608247*avg_height
+w=0.253003533568905*length
+
+x=int(x)
+y=int(y)
+h=int(h)
+w=int(w)
+
+
+cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+end = datetime.datetime.now()
+print end-begin
+#cv2.imshow("RotImg",RotImg)
 
 cv2.imshow("frame",frame)
 cv2.imshow("mask",mask)
