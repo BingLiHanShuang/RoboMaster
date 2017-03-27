947b15816cb81116415d78c9d8b5a492b4eb54fe
wzq <qq960826@gmail.com>
2017-03-27 17:25:05 +0800

    Optimize Code Structure

--- Protocol/stm32-protocol/protocol_sdk.h
+++ Protocol/stm32-protocol/protocol_sdk.h
@@ -4,8 +4,11 @@
 
 #include <stdint.h>
 void serial_send(void *payload, uint32_t size);
+void serial_receive(uint8_t data);
 void callback_ScanResult();
 void callback_PadPass();
 void callback_VideoRecordStatus();
-
+void VideoRecord_Start();//开始录像
+void VideoRecord_Stop();//停止录像
+void VideoRecord_Status();//完成录像
 #endif //STM32_PROTOCOL_SDK_H
