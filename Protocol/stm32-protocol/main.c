c5ebfaad4595b994b46358e87b4e0a2373445e6a
wzq <qq960826@gmail.com>
2017-03-27 17:25:05 +0800

    Optimize Code Structure

--- Protocol/stm32-protocol/main.c
+++ Protocol/stm32-protocol/main.c
@@ -1,19 +1,18 @@
-#include "protocol.h"
+#include "protocol_sdk.h"
 
 #include <stdio.h>
 
 int main() {
     VideoRecord_Start();
     VideoRecord_Stop();
-//    VideoRecord_Status();
-    uart_buffer_index_1=0;
+   VideoRecord_Status();
     uint8_t a[]={0xfe,0x0b,0x00,0x00,0x00,0xc2,0xd9,0x1d,0x47,0x08,0x03,0x12,0x07,0x08,0x01,0x1a,0x03,0x57,0x5a,0x51,0xff}
     ;
     int len= sizeof(a)/sizeof(uint8_t);
     for(int i=0;i<len;i++){
         //uint8_t a=fgetc(fp);
         //printf("%02x ",a);
-        //GetMessage(a[i]);
+        serial_receive(a[i]);
 
     }
 
