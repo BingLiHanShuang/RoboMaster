530c54c56e4d4c93ff2ec855fc8605ddb7e5e6d6
wzq <qq960826@gmail.com>
2017-03-29 09:51:54 +0800

    enhance algorithm robustness

--- PadPass/PadPass_new.py
+++ PadPass/PadPass_new.py
@@ -30,10 +30,10 @@ def cmp_h(x, y):
 green = np.uint8([[[110,140,130 ]]])
 
 hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
-print hsv_green
+#print hsv_green
 
 
-frame=cv2.imread("/Users/wzq/Downloads/MobileFile/IMG_20170317_205659.jpg")
+frame=cv2.imread("/Users/wzq/Downloads/MobileFile/IMG_20170317_205844.jpg")
 begin = datetime.datetime.now()
 
 frame=resize(frame)
@@ -67,13 +67,14 @@ image_dict=sorted(image_dict,cmp=cmp_h)
 image_dict=image_dict[0]
 frame=frame[image_dict[1]:image_dict[1]+image_dict[3],image_dict[0]:image_dict[0]+image_dict[2]]
 mask=mask[image_dict[1]:image_dict[1]+image_dict[3],image_dict[0]:image_dict[0]+image_dict[2]]
-
+#cv2.imshow("aaa",mask)
 
 
 contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # peri = cv2.arcLength(c, True)
 count=0
 sum_angle=0
+angle_set=[]
 for cnt in contours:
     approx = cv2.approxPolyDP(cnt, 0.04 * cv2.arcLength(cnt, True), True)
     if len(approx) == 4 or len(approx)==5:
@@ -83,16 +84,45 @@ for cnt in contours:
         if  w*h<160 or w*h>340:
             continue
         # if(rect[1])
-        sum_angle+=rect[2]
-        count+=1
-        #print w,h,w*h,rect[2]
+        if w>h:
+            angle=rect[2]+90
+        else:
+            angle=rect[2]
+
+
+
         box = cv2.cv.BoxPoints(rect)
         box = np.int0(box)
+
+        mask_rect = np.zeros((mask.shape[0], mask.shape[1]), np.uint8)
+
+        x1, y1, w1, h1 = cv2.boundingRect(cnt)
+        # mask_rect_color= cv2.mean(mask[y1:y1+h1, x1:x1+w1],mask[y1:y1+h1, x1:x1+w1])
+        # cv2.imshow("b",mask[y1:y1+h1, x1:x1+w1])
+        # cv2.waitKey(0)
+        mask_rect_color= mask[y1:y1+h1, x1:x1+w1].mean()
+        # cv2.imshow("a",mask[y1:y1+h1, x1:x1+w1])
+        # cv2.waitKey(0)
+
+        #cv2.drawContours(mask_rect, [box], 0, 255, -1)
+        # x1, y1, w1, h1 = cv2.boundingRect(cnt)
+        # mask_rect_color= cv2.mean(mask[y1:y1+h1, x1:x1+w1],mask_rect[y1:y1+h1, x1:x1+w1])
+        # cv2.imshow("a",mask[y1:y1+h1, x1:x1+w1])
+        # cv2.waitKey(0)
+        print w, h, w * h, angle, mask_rect_color
+        # if mask_rect_color[0]<210:
+        #     continue
+        angle_set.append(angle)
+        sum_angle += angle
+        count += 1
+        #cv2.mean()
         #cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
         #image_dict.append([x, y, w, h])
         #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-RotateMatrix = cv2.getRotationMatrix2D(center=(frame.shape[1]/2, frame.shape[0]/2), angle=sum_angle/count+90, scale=1)
+angle_set.sort(reverse=True)
+angle_average=sum(angle_set[0:10]) / float(10)
+print sum_angle/count
+RotateMatrix = cv2.getRotationMatrix2D(center=(frame.shape[1]/2, frame.shape[0]/2), angle=angle_average, scale=1)
 frame = cv2.warpAffine(frame, RotateMatrix, (frame.shape[0]*2, frame.shape[1]*1))
 mask = cv2.warpAffine(mask, RotateMatrix, (frame.shape[0]*2, frame.shape[1]*1))
 
@@ -108,9 +138,28 @@ for cnt in contours:
     if len(approx) == 4 or len(approx)==5:
         x, y, w, h = cv2.boundingRect(cnt)
         #w,h,x,y=int(rect[0][1]),int(rect[0][1]),int(rect[1][0]),int(rect[1][1])
-        if  w*h<160 or w*h>380:
+        if  w*h<160 or w*h>380 or h>w:
             continue
         # if(rect[1])
+        print w,h,w*h
+
+        mask_rect = np.zeros((mask.shape[0], mask.shape[1]), np.uint8)
+        cv2.rectangle(mask_rect,(x, y), (x + w, y + h),255,-1)
+        # cv2.imshow("mask_rect",mask_rect)
+
+
+        #cv2.drawContours(mask_rect, [box], 0, 255, -1)
+        x1, y1, w1, h1 = cv2.boundingRect(cnt)
+        # mask_rect_color= cv2.mean(mask[y1:y1+h1, x1:x1+w1],mask[y1:y1+h1, x1:x1+w1])
+        # cv2.imshow("a",mask[y1:y1+h1, x1:x1+w1])
+        # cv2.waitKey(0)
+        mask_rect_color= mask[y1:y1+h1, x1:x1+w1].mean()
+        print mask_rect_color
+        #cv2.imshow("a",mask[y1:y1+h1, x1:x1+w1])
+        #cv2.waitKey(0)
+        if(mask_rect_color<180):
+            continue
+
         avg_width+=w
         avg_height+=h
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
@@ -127,24 +176,61 @@ avg_x_right=sum([float(i[0]) for i in pos_rect[5:10]])/5
 pos_rect.sort(key=lambda x:(x[1]))
 avg_y_top=sum([float(i[1]) for i in pos_rect[0:2]])/2
 
-avg_x_left+=avg_width
+length=avg_x_right-avg_x_left
+
+
+#avg_x_left+=avg_width
 print avg_x_left,avg_x_right,avg_width,avg_height
 print pos_rect
 
-length=avg_x_right-avg_x_left
 
-x=0.147703180212014*length+avg_x_left
-y=avg_y_top
-h=2.164948453608247*avg_height
-w=0.253003533568905*length
+def slice_num(num):
+    #paramater
+    original_resolution_width=float(4160)
+    original_resolution_height=float(3120)
+    original_gap_horizontal=float(135)
+    original_gap_vertical=float(60)
+    original_box_width=float(365)
+    oritinal_box_height=float(220)
+    original_signrect_width=float(210)
+    original_signrect_height = float(90)
 
-x=int(x)
-y=int(y)
-h=int(h)
-w=int(w)
+    resize_resolution_width=float(640)
+    resize_resolution_height=float(214)
+    resize_signrect_height=float(avg_height)
+    resize_signrect_width=float(avg_width)
+
+    factor_vertical=resize_signrect_height/original_signrect_height
+    factor_horizontal=resize_signrect_width/original_signrect_width
 
 
-cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+
+
+
+    resize_gap_horizontal=factor_horizontal*original_gap_horizontal
+    resize_gap_gap_vertical=factor_vertical*original_gap_vertical
+    resize_box_width=factor_horizontal*original_box_width
+    resize_box_height=factor_vertical*oritinal_box_height
+
+
+
+    x = (avg_width + resize_gap_horizontal)*(num%3+1) + avg_x_left+resize_gap_horizontal*(num%3)
+    y = avg_y_top+(num/3)*(resize_gap_gap_vertical+resize_box_height)
+    h = resize_box_height
+    w = resize_box_width
+
+    return int(x),int(y),int(w),int(h)
+
+
+
+
+for i in range(0,9):
+    x,y,w,h=slice_num(i)
+
+
+
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 end = datetime.datetime.now()
 print end-begin
