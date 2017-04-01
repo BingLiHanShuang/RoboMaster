2ae7da1ac421e8efd2525cd6978ea6eb879f5507
wzq <qq960826@gmail.com>
2017-04-01 14:40:25 +0800

    update algorithm

---
+++ PadPass/PadPass_new_classifier_test.py
@@ -0,0 +1,14 @@
+from sklearn.externals import joblib
+from skimage.feature import hog
+import numpy as np
+import cv2
+clf = joblib.load("digits_cls.pkl")
+im = cv2.imread("/Users/wzq/RoboMaster/PadPass/test/14.jpg")
+roi = cv2.resize(im, (28, 28), interpolation=cv2.INTER_AREA)
+gray = roi[:,:,0]
+
+roi_hog_fd = hog(gray, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+nbr = clf.predict(np.array([roi_hog_fd], 'float64'))
+print nbr
+cv2.imshow("",roi)
+cv2.waitKey(0)
