6cb31689bb4a25ba072802d3b7e89e232037fbaf
wzq <qq960826@gmail.com>
2017-04-01 10:33:16 +0800

    update algorithm

--- PadPass/PadPass_new1.py
+++ PadPass/PadPass_new1.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import datetime
+from keras.models import model_from_json
+
 def resize(rawimg):  # resize img to 28*28
     fx = 640.0 / rawimg.shape[0]
     fy = 480.0 / rawimg.shape[1]
@@ -33,7 +35,7 @@ hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
 #print hsv_green
 
 
-frame=cv2.imread("/Users/wzq/Downloads/pic_padpass/200.jpg")
+frame=cv2.imread("/Users/wzq/Downloads/pic_padpass/30.jpg")
 begin = datetime.datetime.now()
 
 frame=resize(frame)
@@ -69,7 +71,7 @@ for cnt in contours:
 
         avg_width+=w
         avg_height+=h
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
         pos_rect.append([x,y,w,h])
 
         #image_dict.append([x, y, w, h])
@@ -79,6 +81,8 @@ pos_rect_left=pos_rect[0:5]
 pos_rect_left.sort(key=lambda x:(x[1]))
 pos_rect_right=pos_rect[5:10]
 pos_rect_right.sort(key=lambda x:(x[1]))
+result = []
+
 
 def slice_first_line():
     arg_line=0
@@ -94,9 +98,14 @@ def slice_first_line():
     height+=pos_rect_left[1][1]-y+0.75*pos_rect_left[1][3]
     height+=pos_rect_right[1][1]-y+0.75*pos_rect_right[1][3]
     height=height/2
-
+    y=int(y)
+    x=int(x)
+    height=int(height)
+    length=int(length)
     for i in range(3):
-        cv2.rectangle(frame, (x+ int(length)*i, y), (x + int(length)*(i+1), y + int(height)), (0, 255, 0), 2)
+        result.append(frame.copy()[y:y+height,x+ length*i:x + length*(i+1)])
+
+        #cv2.rectangle(frame, (x+ int(length)*i, y), (x + int(length)*(i+1), y + int(height)), (0, 255, 0), 2)
 
 def slice_second_line():
     arg_line=1
@@ -112,9 +121,13 @@ def slice_second_line():
     height+=pos_rect_left[3][1]-y+0.25*pos_rect_left[3][3]
     height+=pos_rect_right[3][1]-y+0.25*pos_rect_right[3][3]
     height=height/2
-
+    y=int(y)
+    x=int(x)
+    height=int(height)
+    length=int(length)
     for i in range(3):
-        cv2.rectangle(frame, (x+ int(length)*i, int(y)), (x + int(length)*(i+1), int(y) + int(height)), (0, 255, 0), 2)
+        result.append(frame.copy()[y:y+height,x+ length*i:x + length*(i+1)])
+        #cv2.rectangle(frame, (x+ int(length)*i, int(y)), (x + int(length)*(i+1), int(y) + int(height)), (0, 255, 0), 2)
 
 def slice_third_line():
     arg_line=3
@@ -131,83 +144,82 @@ def slice_third_line():
     height+=pos_rect_right[4][1]-y+1*pos_rect_right[4][3]
     height=height/2
 
+    y=int(y)
+    x=int(x)
+    height=int(height)
+    length=int(length)
     for i in range(3):
-        cv2.rectangle(frame, (x+ int(length)*i, int(y)), (x + int(length)*(i+1), int(y) + int(height)), (0, 255, 0), 2)
+        result.append(frame.copy()[y:y+height,x+ length*i:x + length*(i+1)])
+        #cv2.rectangle(frame, (x+ length*i, y), (x + length*(i+1), y+ height), (0, 255, 0), 2)
+    return result
 
 slice_first_line()
 slice_second_line()
 slice_third_line()
-# for i in range(5):
-#     print (pos_rect_left[i][1]-pos_rect_right[i][1])/float(pos_rect_left[i][0]-pos_rect_right[i][0])
-
-avg_width/=10
-avg_height/=10
-avg_x_left=sum([float(i[0]) for i in pos_rect_left])/5
-avg_x_right=sum([float(i[0]) for i in pos_rect_right])/5
-
-pos_rect.sort(key=lambda x:(x[1]))
-avg_y_top=sum([float(i[1]) for i in pos_rect[0:2]])/2
-
-length=avg_x_right-avg_x_left
 
 
-#avg_x_left+=avg_width
-print avg_x_left,avg_x_right,avg_width,avg_height
-print pos_rect
-
-
-# def slice_num(num):
-#     #paramater
-#     original_resolution_width=float(4160)
-#     original_resolution_height=float(3120)
-#     original_gap_horizontal=float(135)
-#     original_gap_vertical=float(60)
-#     original_box_width=float(365)
-#     oritinal_box_height=float(220)
-#     original_signrect_width=float(210)
-#     original_signrect_height = float(90)
-#
-#     resize_resolution_width=float(640)
-#     resize_resolution_height=float(214)
-#     resize_signrect_height=float(avg_height)
-#     resize_signrect_width=float(avg_width)
-#
-#     factor_vertical=resize_signrect_height/original_signrect_height
-#     factor_horizontal=resize_signrect_width/original_signrect_width
-#
-#
-#
-#
-#
-#
-#     resize_gap_horizontal=factor_horizontal*original_gap_horizontal
-#     resize_gap_gap_vertical=factor_vertical*original_gap_vertical
-#     resize_box_width=factor_horizontal*original_box_width
-#     resize_box_height=factor_vertical*oritinal_box_height
-#
-#
-#
-#     x = (avg_width + resize_gap_horizontal)*(num%3+1) + avg_x_left+resize_gap_horizontal*(num%3)
-#     y = avg_y_top+(num/3)*(resize_gap_gap_vertical+resize_box_height)
-#     h = resize_box_height
-#     w = resize_box_width
-#
-#     return int(x),int(y),int(w),int(h)
-#
-#
-#
-#
-# for i in range(0,9):
-#     x,y,w,h=slice_num(i)
-#
-#
-#
-#     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 end = datetime.datetime.now()
 print end-begin
+
+with open('model.json', 'r') as f:
+    model = model_from_json(f.read())
+model.load_weights('model.h5')
+model.summary()
+count=0
+
+
+def resize1(rawimg):  # resize img to 28*28
+    fx = 28.0 / rawimg.shape[0]
+    fy = 28.0 / rawimg.shape[1]
+    fx = fy = min(fx, fy)
+    img = cv2.resize(rawimg, None, fx=fx, fy=fy, interpolation=cv2.INTER_CUBIC)
+    outimg = np.ones((28, 28), dtype=np.uint8) * 255
+    w = img.shape[1]
+    h = img.shape[0]
+    x = (28 - w) / 2
+    y = (28 - h) / 2
+    outimg[y:y+h, x:x+w] = img
+    return outimg
+def recognize(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    res = resize1(gray)
+    res = np.resize(res, (1, 28, 28, 1))
+    predictions = model.predict(res)
+    final=np.argmax(predictions)
+    print final
+    return final
+
+for i in range(9):
+    edge = cv2.Canny(result[i], 500, 2000, apertureSize=5)
+    contours, hierarchy = cv2.findContours(edge.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    copy = result[i].copy()
+    im_gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
+    #im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
+    ret, im_th = cv2.threshold(im_gray, 200, 255, cv2.THRESH_BINARY_INV)
+    print copy.shape
+    for i in contours:
+        x2, y2, w2, h2 = cv2.boundingRect(i)
+        if w2<5 or w2>15 or h2<5:
+            continue
+        # if x2<0.15*w2 or x2>0.85*w2:
+        #     continue
+        # if(w>0.8*w):
+        #     continue
+        img_temp=im_th[y2-1:y2 + h2+1, x2-1:x2 + w2+1].copy()
+
+        cv2.imwrite("test/"+str(count)+".jpg",img_temp)
+        # res=recognize(copy[y2-1:y2 + h2+1, x2-1:x2 + w2+1].copy())
+        # #cv2.rectangle(copy, (x2, y2), (x2 + w2, y2 + h2), (0, 0, 255), 2)
+        count += 1
+        cv2.imshow(str(count), im_gray[y2-1:y2 + h2+1, x2-1:x2 + w2+1].copy())
+
+
+
+
+
 #cv2.imshow("RotImg",RotImg)
 
 cv2.imshow("frame",frame)
-cv2.imshow("mask",mask)
+#cv2.imshow("mask",mask)
 cv2.waitKey(0)
