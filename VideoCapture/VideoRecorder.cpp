bb2404ad474beec0fc551cba52c22cb33ed5c358
wzq <qq960826@gmail.com>
2017-04-02 11:21:28 +0800

    update capture

--- VideoCapture/VideoRecorder.cpp
+++ VideoCapture/VideoRecorder.cpp
@@ -6,13 +6,10 @@
 VideoRecorder videoRecorder;
 VideoRecorder::VideoRecorder(){}
 VideoRecorder::VideoRecorder(int deviceid) {
-    vcap.open(deviceid);
-    if(!vcap.isOpened()){
-        cout<<"Error opening camera id"<<deviceid<<endl;
-        exit(0);
-    }
-    frame_width=vcap.get(CV_CAP_PROP_FRAME_WIDTH);
-    frame_height=vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
+    VideoRecorder::OpenCamera(deviceid);
+}
+VideoRecorder::VideoRecorder(char * devicefile){
+    VideoRecorder::OpenCamera(devicefile);
 }
 void VideoRecorder::OpenCamera(int deviceid){
 
@@ -23,6 +20,40 @@ void VideoRecorder::OpenCamera(int deviceid){
     }
     frame_width=vcap.get(CV_CAP_PROP_FRAME_WIDTH);
     frame_height=vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
+}
+void VideoRecorder::OpenCamera(char * devicefile){
+    width = 640 ;
+    height = 480 ;
+    fps   = 30 ;
+//    filename = "/dev/video0";
+//    avifilename = "test.avi";
+
+    frame_width=width;
+    frame_height=height;
+    int format = V4L2_PIX_FMT_MJPEG;
+    int ret;
+    int grabmethod = 1;
+
+    file = fopen(devicefile, "wb");
+    if(file == NULL)
+    {
+        printf("Unable to open file for raw frame capturing\n ");
+        exit(1);
+    }
+
+    //v4l2 init
+    vd = (struct vdIn *) calloc(1, sizeof(struct vdIn));
+    if(init_videoIn(vd, (char *) devicefile, width, height,fps,format,grabmethod,NULL) < 0)
+    {
+        exit(1);
+    }
+
+    if (video_enable(vd))
+    {
+        exit(1);
+    }
+    camera_status=1;
+
 }
 void VideoRecorder::CloseCamera() {
     if(!vcap.isOpened())vcap.release();
@@ -37,6 +68,21 @@ void VideoRecorder::CreateVideo(string path){
     video.open(path,CV_FOURCC('M','P','4','2'),10,Size(frame_width,frame_height),true);
     frame_count=0;
     status=0;
+}
+void VideoRecorder::CreateVideo1(char *path)
+{
+    vd->avifile = AVI_open_output_file(path);
+    if (vd->avifile == NULL )
+    {
+        printf("Error opening avifile %s\n",vd->avifilename);
+        exit(1);
+    }
+
+    AVI_set_video(vd->avifile, vd->width, vd->height, fps, "MJPG");
+    printf("recording to %s\n",path);
+    frame_count=0;
+    status=0;
+    video_status=1;
 
 }
 Mat VideoRecorder::ReadMat(){
@@ -58,6 +104,33 @@ void VideoRecorder::SaveOneFrame(){
     frame_count++;
     waitKey(1);
 }
+void VideoRecorder::SaveOneFrame1(){
+    int ret;
+
+    memset(&vd->buf, 0, sizeof(struct v4l2_buffer));
+    vd->buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    vd->buf.memory = V4L2_MEMORY_MMAP;
+    ret = ioctl(vd->fd, VIDIOC_DQBUF, &vd->buf);
+    if (ret < 0)
+    {
+        printf("Unable to dequeue buffer");
+        exit(1);
+    }
+
+    memcpy(vd->tmpbuffer, vd->mem[vd->buf.index],vd->buf.bytesused);
+
+    AVI_write_frame(vd->avifile,(char *) vd->tmpbuffer,vd->buf.bytesused, vd->framecount);
+
+    vd->framecount++;
+
+    ret = ioctl(vd->fd, VIDIOC_QBUF, &vd->buf);
+    if (ret < 0)
+    {
+        printf("Unable to requeue buffer");
+        exit(1);
+    }
+
+}
 void VideoRecorder::Recording(){
     while (status==1){
         assert(video.isOpened());
@@ -65,6 +138,22 @@ void VideoRecorder::Recording(){
     }
     if(status==2)
         video.release();
+}
+void VideoRecorder::Release1() {
+    if(video_status==0)
+        return;
+
+    fclose(file);
+    video_status==0;
+}
+void VideoRecorder::Recording1(){
+    while (status==1){
+        assert(camera_status==1);
+        assert(video_status==1);
+        SaveOneFrame1();
+    }
+    if(status==2)
+        Release1();
 }
 VideoRecorder::~VideoRecorder(){
     vcap.release();
