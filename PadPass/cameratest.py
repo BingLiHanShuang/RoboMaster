dfa6bf84dbdc9259cc153a0be77170d76489d757
wzq <qq960826@gmail.com>
2017-04-15 21:13:09 +0800

    add armor

--- PadPass/cameratest.py
+++ PadPass/cameratest.py
@@ -3,7 +3,7 @@ import cv2
 # cap=cv2.VideoCapture("/Users/wzq/Downloads/wzq_1_1491099798.avi")
 
 count=1200
-cap=cv2.VideoCapture(1)
+cap=cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 def resize(rawimg):  # resize img to 28*28
@@ -16,7 +16,7 @@ while True:
     success, frame = cap.read()
     #frame=resize(frame)
     count+=1
-    cv2.imwrite("test/"+str(count)+".jpg",frame)
+    #cv2.imwrite("test/"+str(count)+".jpg",frame)
     cv2.imshow("",frame)
 
     cv2.waitKey(0XFF)
