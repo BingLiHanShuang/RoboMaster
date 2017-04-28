3f021bbea26b2eba1e74f59f1f9104243cae9f13
qq960826 <qq960826@gmail.com>
2017-04-28 13:12:54 +0800

    add video service

---
+++ VideoService/Server/cmake-build-debug/CMakeFiles/VideoServer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoServer.dir/main.cpp.o"
+  "CMakeFiles/VideoServer.dir/Structure.cpp.o"
+  "VideoServer.pdb"
+  "VideoServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
